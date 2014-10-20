05558f9da7e3c593d1de93919a683e1faa782646
Sebastian Jørgensen <sebastian@trendsales.dk>
2014-10-20 16:52:12 +0200

    ..

---
+++ www/scripts/wheels-engine/Viewport.js
@@ -0,0 +1,40 @@
+import Transformation from './Transformation';
+import css from './css';
+
+function createHtml() {
+	var viewport = document.createElement('div'),
+		root = document.createElement('div');
+
+	viewport.classList.add('viewport');
+	root.classList.add('viewport-root');
+	viewport.appendChild(root);
+}
+
+class Viewport {
+	constructor() {
+		var html = createHtml();
+
+		this.__viewportElement = html;
+		this.__rootElement = html.firstChild;
+		this.__transformation = Transformation.identity;
+	}
+
+	get viewportElement() {
+		return this.__viewportElement;
+	}
+
+	get rootElement() {
+		return this.__rootElement;
+	}
+
+	get transformation() { return this.__transformation; }
+
+	set transformation(transformation) {
+		this.__transformation = transformation;
+
+		css.setTransform(this.__rootElement, transformation);
+	}
+}
+
+export default Viewport;
+export const create = (...args) => new Viewport(...args);
